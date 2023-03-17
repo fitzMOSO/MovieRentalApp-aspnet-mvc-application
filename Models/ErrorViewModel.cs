f4c389b5730787d28b2531fcd0a961b559fa1239
fitzg <fitzg@FITZPC>
2023-03-17 12:42:37 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movieRentalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
